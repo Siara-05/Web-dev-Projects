f6c4bb5403f1a156af6af4973482f3894fa0f748
Siara-05 <siara240824@gmail.com>
2025-07-24 22:25:52 +0530

    basic code

--- Darkmode/components/Header/Header.jsx
+++ Darkmode/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <h1>Thhis is header </h1>
+        <h1 className='text-green-900 text-2xl'>Thhis is header </h1>
     </div>
   )
 }
