ad2ff1a6f56a853376dce311089654d00cba1188
Siara-05 <siara240824@gmail.com>
2025-07-25 15:24:13 +0530

    basic design completed

--- Darkmode/src/App.jsx
+++ Darkmode/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import Form from '../components/Form'
+import Form from './components/Form'
 
 
 const App = () => {
   return (
-    <>
-    {/* <h1 className="text-3xl text-green-600 font-bold px-10 m-20">
+    < div  className='min-h-screen bg-gradient-to-t from-blue-100 via-blue-300 to-blue-500'>
+    {/* <h1 className="px-10 m-20 text-3xl font-bold text-green-600">
       Darkmode by react.js and tailwind.css
     </h1> */}
 
     <Form />
     
-    </>
+    </div>
   )
 }
 
