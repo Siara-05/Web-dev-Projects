7d4ee5d6df03c8176e026d6d692620eb70cdd3aa
Siara-05 <siara240824@gmail.com>
2025-07-25 12:20:41 +0530

    form.jsx file

--- Darkmode/components/Form.jsx
+++ Darkmode/components/Form.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
+
 const Form = () => {
   return (
    <div>
-    <form action="">
-        
+    <form action="" className='flex justify-centre bg-gray-500'>
+        <h1 >
+            DarkMode Toggle
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur consectetur amet laborum distinctio, vero dolorum exercitationem at minus pariatur delectus tempore fuga accusantium beatae officia ex, esse consequuntur ratione, expedita cum! Hic dolores soluta repudiandae beatae tenetur corporis quidem.
+        </p>
+
+        <button> change theme </button>
     </form>
    </div>
   )
--- Darkmode/src/App.jsx
+++ Darkmode/src/App.jsx
@@ -5,9 +5,9 @@ import Form from '../components/Form'
 const App = () => {
   return (
     <>
-    <h1 className="text-3xl text-green-600 font-bold px-10 m-20">
+    {/* <h1 className="text-3xl text-green-600 font-bold px-10 m-20">
       Darkmode by react.js and tailwind.css
-    </h1>
+    </h1> */}
 
     <Form />
     
