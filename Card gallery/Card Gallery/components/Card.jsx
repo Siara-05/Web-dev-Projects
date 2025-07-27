773369c64e70f5d31f7bbe895ce72b3d1aea896b
Siara-05 <siara240824@gmail.com>
2025-07-27 23:12:04 +0530

    Card Project Completed

--- Card gallery/Card Gallery/components/Card.jsx
+++ Card gallery/Card Gallery/components/Card.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import './Card.css'
+import Subha from '../src/assets/subha.jpg'
+import Prem from '../src/assets/prem.jpg'
+import Rahul from '../src/assets/rahul.jpg'
+import Om from '../src/assets/om.jpg'
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className=''>
+    <div className='user-container'>
 
-        <p id='name' >  Name</p>
-        <img id='pfp' src="" alt=" pfp" ></img>
-        <p id='prof'>Profession</p>
+        <h2 id='name' > {props.name} </h2>
+        <img id='pfp' src={props.image} alt={props.name}></img>
+        <p id='prof' >  {props.prof} </p>
+        <h6 id='age' className='text-black'> Age- {props.age} </h6>
 
     </div>
   )
--- Card gallery/Card Gallery/src/App.jsx
+++ Card gallery/Card Gallery/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Card from '../components/Card'
+import Subha from '../src/assets/subha.jpg'
+import Prem from '../src/assets/prem.jpg'
+import Rahul from '../src/assets/rahul.jpg'
+import Om from '../src/assets/om.jpg'
 
 function App () {
   return (
-    <div>
-      <h1 className='text-2xl font-bold '> Card gallery by react </h1>
+    <div id='container'>
+      {/* <h1 className='text-2xl font-bold '> Card gallery by react </h1> */}
 
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card name="Subha" prof=" Ui & Frontend Developer" image={Subha} age="19"/> 
+      <Card name="Prem" prof=" AI/Automation Specialist"  image={Prem} age="20"/> 
+      <Card name="Rahul" prof="Researcher" image={Rahul} age="18"/> 
+      <Card name="Om" prof="Gamer" image={Om} age="19"/>
 
 
     </div>
